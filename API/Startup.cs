93e2a479270a7cbcb00aece328c42b22607cfad2
divjha <jhadivanshu@gmail.com>
2020-07-12 20:36:08 +0530

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -26,6 +26,12 @@ namespace API {
             services.AddDbContext<DataContext> (opt => {
                 opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
             });
+            services.AddCors (opt => {
+                opt.AddPolicy ("CorsPolicy", policy => {
+                    policy.AllowAnyHeader ().AllowAnyMethod ().WithOrigins ("http://localhost:3000");
+                });
+            });
+
             services.AddControllers ();
 
         }
@@ -36,7 +42,8 @@ namespace API {
                 app.UseDeveloperExceptionPage ();
             }
 
-            app.UseHttpsRedirection ();
+            //  app.UseHttpsRedirection ();
+            app.UseCors ("CorsPolicy");
 
             app.UseRouting ();
 
